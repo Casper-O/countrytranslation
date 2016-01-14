d0a5c4b167a5e55c1c99ed349799423f31e9abea
Casper <privat@linkcube.dk>
2016-01-14 23:19:36 +0100

    Initial commit

---
+++ countrytranslation.php
@@ -0,0 +1,220 @@
+<?php
+/**
+* 2007-2015 PrestaShop
+*
+* NOTICE OF LICENSE
+*
+* This source file is subject to the Academic Free License (AFL 3.0)
+* that is bundled with this package in the file LICENSE.txt.
+* It is also available through the world-wide-web at this URL:
+* http://opensource.org/licenses/afl-3.0.php
+* If you did not receive a copy of the license and are unable to
+* obtain it through the world-wide-web, please send an email
+* to license@prestashop.com so we can send you a copy immediately.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade PrestaShop to newer
+* versions in the future. If you wish to customize PrestaShop for your
+* needs please refer to http://www.prestashop.com for more information.
+*
+*  @author    PrestaShop SA <contact@prestashop.com>
+*  @copyright 2007-2015 PrestaShop SA
+*  @license   http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+*  International Registered Trademark & Property of PrestaShop SA
+*/
+
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class countrytranslation extends Module
+{
+    protected $config_form = false;
+
+    public function __construct()
+    {
+        $this->name = 'countrytranslation';
+        $this->tab = 'i18n_localization';
+        $this->version = '1.0.0';
+        $this->author = 'Casper Olsen';
+        $this->need_instance = 0;
+
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
+
+        parent::__construct();
+
+        $this->displayName = $this->l('Country Translation');
+        $this->description = $this->l('Translate country names with a simple click');
+
+        $this->confirmUninstall = $this->l('');
+
+        $this->ps_versions_compliancy = array('min' => '1.6', 'max' => _PS_VERSION_);
+    }
+
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
+    public function install()
+    {
+        return parent::install();
+    }
+
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
+
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        /**
+         * If values have been submitted in the form, process.
+         */
+        if (((bool)Tools::isSubmit('submitCountryTranslationModule')) == true) {
+            $this->postProcess();
+        }
+		
+        return $this->renderForm();
+    }
+
+    /**
+     * Create the form that will be displayed in the configuration of your module.
+     */
+    protected function renderForm()
+    {
+        $helper = new HelperForm();
+
+        $helper->show_toolbar = false;
+        $helper->module = $this;
+        $helper->default_form_language = $this->context->language->id;
+        $helper->allow_employee_form_lang = Configuration::get('PS_BO_ALLOW_EMPLOYEE_FORM_LANG', 0);
+
+        $helper->identifier = $this->identifier;
+        $helper->submit_action = 'submitCountryTranslationModule';
+        $helper->currentIndex = $this->context->link->getAdminLink('AdminModules', false)
+            .'&configure='.$this->name.'&tab_module='.$this->tab.'&module_name='.$this->name;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+
+        $helper->tpl_vars = array(
+            'languages' => $this->context->controller->getLanguages(),
+            'id_language' => $this->context->language->id,
+        );
+
+        return $helper->generateForm(array($this->getConfigForm()));
+    }
+
+    /**
+     * Create the structure of your form.
+     */
+    protected function getConfigForm()
+    {
+        return array(
+            'form' => array(
+                'legend' => array(
+                'title' => $this->l('Settings'),
+                'icon' => 'icon-cogs',
+                ),
+                'input' => array(
+                    array(
+                        'type' => 'switch',
+                        'label' => $this->l('Live mode'),
+                        'name' => 'COUNTRY TRANSLATION_LIVE_MODE',
+                        'is_bool' => true,
+                        'desc' => $this->l('Use this module in live mode'),
+                        'values' => array(
+                            array(
+                                'id' => 'active_on',
+                                'value' => true,
+                                'label' => $this->l('Enabled')
+                            ),
+                            array(
+                                'id' => 'active_off',
+                                'value' => false,
+                                'label' => $this->l('Disabled')
+                            )
+                        ),
+                    ),
+                ),
+                'submit' => array(
+                    'title' => $this->l('Save'),
+                ),
+            ),
+        );
+    }
+
+    /**
+     * Set values for the inputs.
+     */
+    protected function getConfigFormValues()
+    {
+		
+    }
+
+    /**
+     * Save form data.
+     */
+    protected function postProcess()
+    {
+    	$languages = Language::getLanguages();
+    	$row = 1;
+    	//ppp($this->_path);
+    	$firstline = true;
+    	if (($handle = fopen($this->local_path."/langs/IP2LOCATION-COUNTRY-MULTILINGUAL.CSV", "r")) !== FALSE) {
+    		while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+	    		if($firstline) { $firstline = false; continue; }
+	    		
+	    		$num = count($data);
+	    		$result[$data[0]][] = array(
+	    			'iso_code' => $data[2],
+	    			'country_name' => ucwords(strtolower($data[5]))
+	    		);
+		
+	    	}
+	    	
+	    	foreach($languages as $language)
+	    	{
+	    		{
+		    		foreach ($result[strtoupper($language['iso_code'])] as $row)
+		    		{
+						$data = array(
+							'name' => $row['country_name'],
+							'id' => (string)$row['iso_code']
+						);
+						
+						DB::getInstance()->execute("UPDATE "._DB_PREFIX_."country_lang SET name = '".$data['name']."' WHERE id_lang = ".$language['id_lang']." AND id_country = (SELECT c.id_country FROM "._DB_PREFIX_."country c WHERE c.iso_code = '".$data['id']."')");
+					}
+	    		}
+	    	}
+    	}
+		/*
+		$languages = Language::getLanguages();
+		foreach($languages as $language)
+		{
+			ppp(dirname(__FILE__));
+			ppp($language);
+			$lang_path = $this->local_path.'langs/'.$language['iso_code'].'.xml';
+			if(is_file($lang_path))
+			{
+				$countries= simplexml_load_file($lang_path);
+				echo count($countries);
+				foreach ($countries as $row)
+				{
+					$data = array(
+						'name' => (string)$row->name,
+						'id' => (string)$row->attributes()->id
+					);
+					DB::getInstance()->execute("UPDATE "._DB_PREFIX_."country_lang SET name = '".$data['name']."' WHERE id_lang = ".$language['id_lang']." AND id_country = (SELECT c.id_country FROM "._DB_PREFIX_."country c WHERE c.iso_code = '".$data['id']."')");
+				}
+			}
+		}
+		ddd();
+		*/
+    }
+}
